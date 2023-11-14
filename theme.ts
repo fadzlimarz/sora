365703de764357b8fac3e9374eb92c3fd1b25b94
Fadzli Marzuki <fadzli@87-92-97-250.bb.dnainternet.fi>
2023-11-14 21:08:58 +0200

    Changed fonts

--- theme.ts
+++ theme.ts
@@ -3,5 +3,8 @@
 import {createTheme} from '@mantine/core'
 
 export const theme = createTheme({
+  fontFamily: 'Noto Sans, sans-serif',
+  fontFamilyMonospace: 'Noto Sans, sans-serif',
+  headings: {fontFamily: 'Noto Sans, sans-serif'},
   primaryColor: 'gray'
 })
